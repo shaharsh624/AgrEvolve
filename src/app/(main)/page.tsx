9d0b20ebcec2e295553296c5cb45b0bd75a50d43
Vandeet Shah <vandeet0703@gmail.com>
2024-04-15 13:08:10 +0530

    Frontend done

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import App from "@/components/map/app";
+import { TypewriterEffectDemo } from "@/components/homepage";
 export default function Home() {
     return (
         <main className="flex flex-col items-center justify-between p-24">
-            Main
-            {/* <div className="w-[500px] h-[500px]">
-        <App />
-      </div> */}
+            <TypewriterEffectDemo />
         </main>
     );
 }
---
+++ src/components/homepage.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { TypewriterEffect } from "@/components/aceternity/typewriter-effect";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+export function TypewriterEffectDemo() {
+  const words = [
+    {
+      text: "Market",
+    },
+    {
+      text: "Data",
+    },
+    {
+      text: "Simplified",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "AgrEvolve.",
+      className: "text-green-500 dark:text-green-500",
+    },
+  ];
+  return (
+    <div className="flex flex-col items-center mt-40 h-[40rem] ">
+      <p className="text-neutral-600 dark:text-neutral-200 text-base  mb-10">
+        One stop solution for all Information related to Agriculture
+      </p>
+      <TypewriterEffect words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 mt-10">
+        <Link href="/commodities">
+        <Button variant="outline">Get Started</Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
